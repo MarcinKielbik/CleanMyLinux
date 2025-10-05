1532946ffabb2206aa84fb4f7d11d002724176f7
Marcin Kielbik <marcinlukasz.kielbik@gmail.com>
2025-10-05 17:25:14 +0200

    Enhance Cleaner to report freed space

--- src/main/java/pl/marcinkielbik/Cleaner.java
+++ src/main/java/pl/marcinkielbik/Cleaner.java
@@ -1,6 +1,7 @@
 package pl.marcinkielbik;
 
 import java.io.File;
+import java.text.DecimalFormat;
 
 public class Cleaner {
 
@@ -35,6 +36,17 @@ public class Cleaner {
           return 0;
       }
 
+    }
+
+    protected String readableFileSize(long size) {
+        if (size == 0) {
+            return "0.0 B";
+        }
+
+        final String[] units = {"B", "KB", "MB", "GB", "TB"};
+
+        int digitGroups = (int) (Math.log10(size) / Math.log10(1024));
+        return new DecimalFormat("#,##0.#").format(size / Math.pow(1024, digitGroups)) + " " + units[digitGroups];
     }
 
 }
--- src/main/java/pl/marcinkielbik/Main.java
+++ src/main/java/pl/marcinkielbik/Main.java
@@ -9,12 +9,13 @@ public class Main {
         String home = System.getProperty("user.home");
         String trash = home + "/.local/share/Trash/files";
 
-
         System.out.println("=== Clean My Linux ===");
+
         long freed = 0;
 
         freed += cleaner.cleanDirectory(trash);
 
+        System.out.println("Total freed space: " + cleaner.readableFileSize(freed));
         System.out.println("Done ✅");
     }
 }
